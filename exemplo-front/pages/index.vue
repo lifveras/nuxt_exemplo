ffe0ee66ed982bce4ed0d40c4bddf8087e84edc5
Professor <gustavo_veras@ifsp.edu.br>
2022-05-02 16:41:42 -0300

    Criação exemplo Nuxt.js

---
+++ exemplo-front/pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>  
+    <h1>Bem vindo ao Nuxt.js</h1>
+    <ul>
+      <li>Elemento 1</li>
+      <li>Elemento 2</li>
+      <li>Elemento 3</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage'
+}
+</script>
+
+<style scoped>
+  h1{
+    color: blueviolet;
+  }
+</style>
---
+++ exemplo-front/pages/lista_pessoas.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="container">
+      <h1>{{titulo}}</h1>
+      <ul>
+          <li :key="item.id" v-for = "item in pessoas">
+              <!-- Saída: Marcio -> 18 anos -->
+              {{item.nome}} -> {{item.idade}} anos
+          </li>
+      </ul>
+      <button>Clique aqui</button>
+  </div>
+</template>
+
+<script>
+export default {
+    data: function (){
+        return {
+            titulo: "Lista de pessoas e idades",
+            pessoas: [
+                {id: 1, nome: "Marcio", idade: 18},
+                {id: 2, nome: "Carla", idade: 25},
+                {id: 3, nome: "Lais", idade: 30},
+                {id: 4, nome: "Carla", idade: 25},
+                {id: 5, nome: "Lais", idade: 30},
+                {id: 6, nome: "Carla", idade: 25},
+                {id: 7, nome: "Lais", idade: 30},
+                {id: 8, nome: "Carla", idade: 25},
+            ]
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
